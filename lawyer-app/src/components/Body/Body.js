57ef93ef98aecb3f9f401eeddcbfd59719e71fc9
Edward <eduard.tymoshuk@gmail.com>
2020-01-21 13:34:38 +0200

    body grid

--- lawyer-app/src/components/Body/Body.js
+++ lawyer-app/src/components/Body/Body.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileAlt, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import './Body.css';
 
 const Body = (props) => {
- return (
-     <div>
-         <div>
-             
-         </div>
-     </div>
- )
+    return (
+        <div>
+            <div className="body-menu container-fluid">
+                <div className="body-menu-cases">
+                    <FontAwesomeIcon icon={faFileAlt} size="6x"/>
+                </div>
+                <div className="body-menu-add">
+                    <FontAwesomeIcon icon={faPlus} size="6x"/>
+                </div>
+                <div className="body-menu-search">
+                    <FontAwesomeIcon icon={faSearch} size="6x"/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Body;
