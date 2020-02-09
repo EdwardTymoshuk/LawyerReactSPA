cdde033850cdeca6b0242341c60abe1425306ff5
Edward <eduard.tymoshuk@gmail.com>
2020-02-10 00:08:21 +0200

    Preloader, mapDispatchToProps refactoring

--- lawyer-app/src/components/Cases/CasesContainer.js
+++ lawyer-app/src/components/Cases/CasesContainer.js
@@ -1,35 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setCases } from '../../redux/casesReducer';
+import { setCases, toggleIsFetching } from '../../redux/casesReducer';
 import Cases from './Cases';
 import * as axios from 'axios';
+import Preloader from '../common/Preloader';
+
 
 class CasesContainer extends React.Component {
     componentDidMount() {
-        if (this.props.cases.length === 0) {
-            axios.get('http://localhost:3000/cases').then(response => {
-                this.props.setCases(response.data);
-            }
-            )
+        this.props.toggleIsFetching(true);
+        axios.get('http://localhost:3000/cases').then(response => {
+            this.props.toggleIsFetching(false);
+            this.props.setCases(response.data);
         }
+        )
     }
     render() {
-        return <Cases cases={this.props.cases} />
+        return <>
+            {this.props.isFetching ? <Preloader />  : null}
+            <Cases cases={this.props.cases}
+            />
+        </>
     }
 }
 
 let mapStateToProps = (state) => {
     return {
-        cases: state.casesPage.cases
+        cases: state.casesPage.cases,
+        isFetching: state.casesPage.isFetching
     }
 }
 
-let mapDispatchToProps = (dispatch) => {
-    return {
-        setCases: (cases) => {
-            dispatch(setCases(cases));
-        }
-    }
-}
+// let mapDispatchToProps = (dispatch) => {
+//     return {
+//         setCases: (cases) => {
+//             dispatch(setCases(cases));
+//         },
+//         toggleIsFetching: (isFetching) => {
+//             dispatch(toggleIsFetching(isFetching));
+//         }
+//     }
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CasesContainer)
+export default connect(mapStateToProps, {setCases, toggleIsFetching})(CasesContainer)
--- lawyer-app/src/redux/casesReducer.js
+++ lawyer-app/src/redux/casesReducer.js
@@ -1,79 +1,41 @@
-const SET_CASES = "SET_CASES";
+const SET_CASES = 'SET_CASES';
 const ADD_CASE = 'ADD_CASE';
 const UPDATE_NEW_CASE = 'UPDATE_NEW_CASE';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 let initialState = {
-    cases: [
-        //   {
-        //       id: 1,
-        //       nrRef: 'a1',
-        //       firstName: 'Roberto',
-        //       secondName: 'Firminho',
-        //       dob: '11.11.11',
-        //       adress: 'Brazilia',
-        //       description: 'Si Senior',
-        //   },
-        //   {
-        //       id: 2,
-        //       nrRef: 'a2',
-        //       firstName: 'Mohamed',
-        //       secondName: 'Salah',
-        //       dob: '1.11.12',
-        //       adress: 'Egypt',
-        //       description: 'Egyption King',
-        //   },
-        //   {
-        //       id: 3,
-        //       nrRef: 'a3',
-        //       firstName: 'Sadio',
-        //       secondName: 'Mane',
-        //       dob: '10.01.86',
-        //       adress: 'Senegal',
-        //       description: 'Senegal dad',
-        //   },
-        //   {
-        //       id: 4,
-        //       nrRef: 'a4',
-        //       firstName: 'Jordan',
-        //       secondName: 'Henderson',
-        //       dob: '12.04.77',
-        //       adress: 'England',
-        //       description: 'Captain',
-        //   }
-    ]
+  cases: [],
+  isFetching: false
 }
 
 const casesReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case SET_CASES:
-            return {...state, cases: [...state.cases, ...action.cases]}
-            case ADD_CASE:
-          let body =  state.newCase;
-          return {
-            ...state,
-            newCase: {},
-            cases: [...state.cases, body]
-          }
-        case UPDATE_NEW_CASE:
-            return {
-              ...state,
-              newCase: action.newCase
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_CASES:
+      return { ...state, cases: [...state.cases, ...action.cases] }
+    case ADD_CASE:
+      let body = state.newCase;
+      return {
+        ...state,
+        newCase: {},
+        cases: [...state.cases, body]
+      }
+    case UPDATE_NEW_CASE:
+      return {
+        ...state,
+        newCase: action.newCase
+      }
+    case TOGGLE_IS_FETCHING:
+      return {
+        ...state,
+        isFetching: action.isFetching
+      }
+    default:
+      return state;
+  }
 }
 export const setCases = (cases) => ({ type: SET_CASES, cases });
-export const addCaseAC = () => {
-    return {
-      type: ADD_CASE
-    }
-  };
-  export const updateNewCaseAC = (newCase) => {
-    return {
-      type: UPDATE_NEW_CASE,
-      newCase
-    }
-  };
+export const addCaseAC = () => ({ type: ADD_CASE });
+export const updateNewCaseAC = (newCase) => ({ type: UPDATE_NEW_CASE, newCase });
+export const toggleIsFetching = (isFetching) => ({ type: TOGGLE_IS_FETCHING, isFetching });
 
 export default casesReducer;
