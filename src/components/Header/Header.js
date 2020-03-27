08f07972f991d5246b0d9482685465b410e88742
Edward Tymoshuk <eduard.tymoshuk@gmail.com>
2020-03-27 20:04:19 +0100

    animation of search input

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import {Navbar, Form, FormControl, Button, Nav} from 'react-bootstrap';
 import {NavLink, Link} from 'react-router-dom';
 
 const Header = (props) => {
+  const [openSearch, setOpenSearch] = useState(false);
     return (
         <Navbar expand="lg" variant="dark" fixed="top" className="header">
         <Navbar.Brand className="header-brand" >LAWYER REACT APP</Navbar.Brand>
         <Nav.Link className="header-cases"><Button><Link to={'/'}>Cases</Link></Button></Nav.Link>
           <Form inline className="header-search">
-            <FormControl type="text" placeholder="Search" className="mr-sm-2 header-search-input" />
-            <Button className="header-search-btn">Search</Button>
+            {
+            <FormControl type="text" placeholder="Search" className={`mr-sm-2 header-search-input ${openSearch ? "header-search-visible" : "header-search-hiden"}`} />
+          }
+            <Button className="header-search-btn" onClick={() => setOpenSearch(!openSearch)}>Search</Button>
           </Form>
           <Nav.Link className="header-login">
             {props.isAuth 
