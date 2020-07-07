84046001725d329af3cca525cd8dd7a19aed8241
Eduard Tymoshuk <edward_t@MacBook-Pro-Edward.local>
2020-07-07 16:14:34 +0200

    Updates

---
+++ precache-manifest.836e3deef62ffb4d872d989e012f24c0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ce582d36841898cd43b3f78d94e625",
+    "url": "/LawyerReactSPA/index.html"
+  },
+  {
+    "revision": "8f309b15d6a23520cc48",
+    "url": "/LawyerReactSPA/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "55b5d39f1147d29166c5",
+    "url": "/LawyerReactSPA/static/css/3.3db4cf8d.chunk.css"
+  },
+  {
+    "revision": "ec7dd298974308fbde59",
+    "url": "/LawyerReactSPA/static/css/4.48d28976.chunk.css"
+  },
+  {
+    "revision": "f4ebade2841a35b34941",
+    "url": "/LawyerReactSPA/static/css/main.b9e74925.chunk.css"
+  },
+  {
+    "revision": "8f309b15d6a23520cc48",
+    "url": "/LawyerReactSPA/static/js/2.59bf5472.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/LawyerReactSPA/static/js/2.59bf5472.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55b5d39f1147d29166c5",
+    "url": "/LawyerReactSPA/static/js/3.acfd93cb.chunk.js"
+  },
+  {
+    "revision": "ec7dd298974308fbde59",
+    "url": "/LawyerReactSPA/static/js/4.5d83d081.chunk.js"
+  },
+  {
+    "revision": "5b9dcf9ff9363ca4959f",
+    "url": "/LawyerReactSPA/static/js/5.87547bc4.chunk.js"
+  },
+  {
+    "revision": "f4ebade2841a35b34941",
+    "url": "/LawyerReactSPA/static/js/main.eb945f62.chunk.js"
+  },
+  {
+    "revision": "24ef12597e7cc8989a4b",
+    "url": "/LawyerReactSPA/static/js/runtime-main.f86e5938.js"
+  },
+  {
+    "revision": "b71c9e01631ed0c286f3d67693079e98",
+    "url": "/LawyerReactSPA/static/media/preloader.b71c9e01.svg"
+  }
+]);
